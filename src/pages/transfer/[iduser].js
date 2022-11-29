dd2aba0bce3f2cad9e444787224c6499292d7119
rofiq999 <rofiqhidayat495@gmail.com>
2022-11-29 17:47:20 +0700

    erorr handler transfer

--- src/pages/transfer/[iduser].js
+++ src/pages/transfer/[iduser].js
@@ -68,6 +68,9 @@ function TransferID() {
   };
 
   const clickHandler = () => {
+    if(price < 10000) {
+      return toast.error("minimum RP. 10.000")
+    }
     if (profile.balance < price) {
       return toast.error("overlimit");
     }
